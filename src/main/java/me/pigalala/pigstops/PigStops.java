94cb04d937f407445cf332772b14778b45a9f77e
Pigalala <w8888mc@gmail.com>
2022-09-18 18:31:19 +0100

    import

---
+++ src/main/java/me/pigalala/pigstops/PigStops.java
@@ -0,0 +1,12 @@
+package me.pigalala.pigstops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PigStops extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
