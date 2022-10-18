080655ef38134865f77ddb7cf7f8b639d6a66c66
Pigalala <w8888mc@gmail.com>
2022-10-18 18:46:18 +0100

    oops

--- src/main/java/me/pigalala/pigstops/annotations/PitVariantHandler.java
+++ src/main/java/me/pigalala/pigstops/annotations/PitVariantHandler.java
@@ -5,13 +5,11 @@ public class PitVariantHandler {
     public PitVariantHandler(Class<?> pitVariant) {
         if(pitVariant.isAnnotationPresent(PitVariant.class)) {
             PitVariant pitVariantValues = pitVariant.getAnnotation(PitVariant.class);
-            inventoryName = pitVariantValues.inventoryName();
             itemsToClick = pitVariantValues.itemsToClick();
             inventorySize = pitVariantValues.inventorySize();
         }
     }
 
-    public String inventoryName;
     public int itemsToClick;
     public int inventorySize;
 }
